7cc25c3fa8e372b12467c93849a1244b9a1a8f47
wod <dmitriyvolchenko3003@gmail.com>
2023-12-30 19:21:20 +0200

    update paths

--- src/shared/api/paths.ts
+++ src/shared/api/paths.ts
@@ -1,13 +1,13 @@
 export const API_PATHS = {
-  register: 'auth/register',
-  login: 'auth/login',
-  verifyEmail: 'auth/verify-email',
-  forgotPassword: 'auth/forgot-password',
-  resetPassword: 'auth/reset-password',
-  getMe: 'auth/me',
+  register: '/auth/register',
+  login: '/auth/login',
+  verifyEmail: '/auth/verify-email',
+  forgotPassword: '/auth/forgot-password',
+  resetPassword: '/auth/reset-password',
+  getMe: '/auth/me',
 
-  seekers: 'seekers', // candidates
-  employers: 'employers', // companies
-  resumes: 'resumes',
-  jobs: 'jobs',
+  seekers: '/seekers', // candidates
+  employers: '/employers', // companies
+  resumes: '/resumes',
+  jobs: '/jobs',
 };
