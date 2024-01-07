22a4c25a40d102d27b2e24ba811383f501bf34aa
wod <dmitriyvolchenko3003@gmail.com>
2024-01-07 16:21:18 +0200

    update filter

--- src/features/jobs/components/Jobs/Jobs.tsx
+++ src/features/jobs/components/Jobs/Jobs.tsx
@@ -1,4 +1,4 @@
-import { Stack } from '@mantine/core';
+import { Stack, Grid } from '@mantine/core';
 
 import { useAppSelector } from 'store/hooks';
 import {
@@ -6,16 +6,24 @@ import {
   // selectIsLoading
 } from '../../jobsSlice';
 import { JobCard } from '../JobCard';
+import { JobsFilter } from '../Filter';
 
 export default function Jobs() {
   const jobs = useAppSelector(selectJobs);
   // const isLoading = useAppSelector(selectIsLoading);
 
   return (
-    <Stack gap={24}>
-      {jobs.map((job) => (
-        <JobCard key={job._id} job={job} />
-      ))}
-    </Stack>
+    <Grid>
+      <Grid.Col span={9}>
+        <Stack gap={24}>
+          {jobs.map((job) => (
+            <JobCard key={job._id} job={job} />
+          ))}
+        </Stack>
+      </Grid.Col>
+      <Grid.Col span={3}>
+        <JobsFilter />
+      </Grid.Col>
+    </Grid>
   );
 }
