6582ade1872368f86e657f4d812b5b61bf916ba2
wod <dmitriyvolchenko3003@gmail.com>
2024-02-01 20:02:34 +0200

    update libs

--- src/features/user/components/SignUp/schema.ts
+++ src/features/user/components/SignUp/schema.ts
@@ -3,17 +3,26 @@ import { i18n } from 'shared/i18n';
 
 export const signUpSchema = z
   .object({
-    firstName: z.string().min(2, {
-      message: i18n.t('sv_first_name_should_have_at_least_2_letters'),
-    }),
-    lastName: z.string().min(2, {
-      message: i18n.t('sv_last_name_should_have_at_least_2_letters'),
-    }),
+    firstName: z
+      .string()
+      .trim()
+      .min(2, {
+        message: i18n.t('sv_first_name_should_have_at_least_2_letters'),
+      }),
+    lastName: z
+      .string()
+      .trim()
+      .min(2, {
+        message: i18n.t('sv_last_name_should_have_at_least_2_letters'),
+      }),
     email: z.string().email({ message: i18n.t('sv_invalid_email') }),
-    password: z.string().min(8, {
-      message: i18n.t('sv_password_should_have_at_least_8_characters'),
-    }),
-    confirmPassword: z.string(),
+    password: z
+      .string()
+      .trim()
+      .min(8, {
+        message: i18n.t('sv_password_should_have_at_least_8_characters'),
+      }),
+    confirmPassword: z.string().trim(),
     terms: z.literal(true, {
       errorMap: () => ({
         message: i18n.t('sv_you_must_accept_the_terms_conditions'),
